61a45c668eb164b3b0b8a471b6258649ecf51146
Ritik Pawar <ritikpawar354@gmail.com>
2025-04-11 16:29:33 +0530

    changes

--- script.js
+++ script.js
@@ -1,7 +1,64 @@
+/* particles */
+const particle = ()=> {const count = 200,
+  defaults = {
+    origin: { y: 0.7 },
+  };
+
+function fire(particleRatio, opts) {
+  confetti(
+    Object.assign({}, defaults, opts, {
+      particleCount: Math.floor(count * particleRatio),
+    })
+  );
+}
+
+fire(0.25, {
+  spread: 26,
+  startVelocity: 55,
+});
+
+fire(0.2, {
+  spread: 60,
+});
+
+fire(0.35, {
+  spread: 100,
+  decay: 0.91,
+  scalar: 0.8,
+});
+
+fire(0.1, {
+  spread: 120,
+  startVelocity: 25,
+  decay: 0.92,
+  scalar: 1.2,
+});
+
+fire(0.1, {
+  spread: 120,
+  startVelocity: 45,
+});
+}
 let add = document.querySelector("#add");
 
 let tasks = [];
 
+/* retrieval of tasks */
+
+document.addEventListener("DOMContentLoaded",()=>{
+    const storedTasks = JSON.parse(localStorage.getItem('tasks'));
+
+    if(storedTasks){
+        storedTasks.forEach((task)=>tasks.push(task));
+        updateStats();
+        updateTaskList();
+    }
+})
+/* save task to local storage */
+const saveTasks =()=> {
+    localStorage.setItem('tasks',JSON.stringify(tasks));
+    
+};
 /* add task */
 const addTask = () => {
     let input = document.querySelector("#task");
@@ -11,6 +68,7 @@ const addTask = () => {
         updateTaskList();
         input.value="";
         updateStats();
+        saveTasks();
     }
     
     
@@ -20,6 +78,7 @@ const deleteTask = (index)=>{
     tasks.splice(index,1);
     updateTaskList();
     updateStats();
+    saveTasks();
 }
 /* edit task */
 const editTask = (index)=>{
@@ -28,6 +87,7 @@ const editTask = (index)=>{
     tasks.splice(index,1);
     updateTaskList();
     updateStats();
+    saveTasks();
 }
 /* update stats */
 const updateStats = () =>{
@@ -40,12 +100,18 @@ const updateStats = () =>{
     /* update stats */
     const stats = document.getElementById(number);
     number.innerHTML =`${completedTasks }/${ totalTasks}`
+    /* particle */
+    if(tasks.length && totalTasks === completedTasks){
+        particle();
+    }
 
 }
 const toggleTaskComplete = (index) =>{
     tasks[index].completed = !tasks[index].completed;
     updateTaskList();
     updateStats();
+    saveTasks();
+    
 }
 /* update task list */
 let updateTaskList = (index) =>{
